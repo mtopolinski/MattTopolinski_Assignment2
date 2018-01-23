786a77f7171ebd5f89c69662321370d1ae5ff984
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-01-23 08:59:50 -0600

    Add files via upload

---
+++ Reverse.js
@@ -0,0 +1,25 @@
+/*
+
+Write a function called ReveseString() that takes one argument You may assume that the argument is a string.
+o Return the a string with all of the characters in reverse order.
+o For example: the string "reverse" will return "esrever".
+o What happens if null is passed in as an argument? How can we prevent problems caused by null or undefined?
+
+*/
+
+//function to accept String and reverse it
+function ReverseString(textVar)
+{
+    //initializing an array to accept each character from the original String
+    var textBack = [];
+
+    for (i = 0; i < textVar.length; i++)
+    {
+
+
+    }
+
+    //return the array with the text reversed
+    return textBack;
+
+}
