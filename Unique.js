d329b1031acf971d881e87335729d04d714a59c6
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-01-23 09:55:06 -0600

    Add files via upload

---
+++ Unique.js
@@ -0,0 +1,13 @@
+/*
+
+Create a JavaScript file called Unique.js that will do the following.
+ Write a function called IsUnique() that takes an array as an argument You may assume that the array has only strings.
+o Return true is every element in the array is unique.
+o Return false is two or more elements are the same.
+
+ */
+
+function isUnique()
+{
+
+}
