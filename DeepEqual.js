5de46126fce163628a7b6044093e1ee4f7f0d4d1
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-01-23 12:48:44 -0600

    Add files via upload

--- DeepEqual.js
+++ DeepEqual.js
@@ -1,20 +1,31 @@
 /*
 
-Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal when compared with a recursive call to deepEqual.
-To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
+Write a function, deepEqual, that takes two values and returns true only if they are the same value or are objects with the same properties whose values are also equal
+when compared with a recursive call to deepEqual.
+
+To find out whether to compare two things by identity (use the === operator for that) or by looking at their properties, you can use the typeof operator.
+If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: by a historical accident, typeof null also produces "object".
+
 // Your code here.
+
 var obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
 // → true
+
 console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
+
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
 
 
  */
 
-function DeepEqual(userObj)
+function DeepEqual(input1, input2)
 {
-    
+
+    if (input1 === null || input2 === null)
+    {
+        return "Error; a null and/or null object has been used.";
+    }
 }
